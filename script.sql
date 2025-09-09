05bbd932d876c3cf9b5df2a498bd39ffb88710ee
Lucas Antonio <lucasklsz1@gmail.com>
2025-09-09 11:22:08 -0300

    learn(ap10): encerra o codigo 2.4.6
    -retomar vendo while

---
+++ script.sql
@@ -0,0 +1,85 @@
+--VERIFICAR ERRO:
+-- DO $$
+-- DECLARE
+--   i INT;
+--   j INT;
+-- BEGIN
+--     i := 0
+--     <<externo>>
+--     LOOP
+--         i := i + 1;
+--         EXIT WHEN i > 10;
+--         j := 1;
+--         <<interno>>
+--         LOOP
+--             RAISE NOTICE "%, %", i, j;
+--             j := j + 1;
+--             EXIT WHEN j > 10;
+--             EXIT externo WHEN j > 5;
+--         END LOOP;
+--     END LOOP;
+-- END
+-- $$
+
+
+
+
+-- DO $$
+-- DECLARE
+--     contador INT := 0;
+-- BEGIN
+--     LOOP
+--       contador := contador +1;
+--       EXIT WHEN contador > 100;
+--       IF contador % 7 = 0 THEN
+--         CONTINUE; -- não execute o restabte da iteração pg_collation_actual_version
+--       END IF;
+--       CONTINUE WHEN contador % 11 = 0;
+--       RAISE NOTICE '%', contador;
+--     END LOOP; 
+-- END;
+-- $$
+
+
+
+--EXIT/WHEN
+-- DO $$
+-- DECLARE
+--     contador INT := 1;
+-- BEGIN  
+--     LOOP
+--       RAISE NOTICE '%', contador;
+--       contador := contador +1;
+--       EXIT WHEN contador > 10;
+--     END LOOP;
+-- END;
+-- $$
+
+
+
+
+
+-- DO $$
+-- DECLARE
+--     contador INT:= 1;
+-- BEGIN
+--     LOOP
+--       RAISE NOTICE '%', contador;
+--       contador := contador + 1;
+--       IF contador > 10 THEN
+--        EXIT;
+--       END IF; 
+--     END LOOP;
+-- END
+-- $$
+
+
+
+
+-- DO $$
+-- BEGIN
+--     LOOP
+--       RAISE NOTICE 'Testando nosso primeiro loop...';
+--     END LOOP;
+-- END;
+-- $$
